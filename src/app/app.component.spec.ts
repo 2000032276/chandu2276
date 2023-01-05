f7e41dc62a13b3e00c7ef02f557dbe1061454ef2
Your Name <you@example.com>
2023-01-05 16:36:51 +0530

    done

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'chandu2276'`, () => {
+  it(`should have as title 'Space'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('chandu2276');
+    expect(app.title).toEqual('Space');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('chandu2276 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Space app is running!');
   });
 });
